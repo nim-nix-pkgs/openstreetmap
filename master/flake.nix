5cdb470b6c4b5b839a5fbfd8e635e05694cdfe02
hugosenari <hugosenari@gmail.com>
2022-03-27 18:25:20 -0300

    feat(nimpkgs): remove lock

--- master/flake.nix
+++ master/flake.nix
@@ -11,6 +11,7 @@
   inputs.src-openstreetmap-master.ref   = "refs/heads/master";
   inputs.src-openstreetmap-master.owner = "juancarlospaco";
   inputs.src-openstreetmap-master.repo  = "nim-openstreetmap";
+  inputs.src-openstreetmap-master.dir   = "";
   inputs.src-openstreetmap-master.type  = "github";
   
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
